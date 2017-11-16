d9dc6d50da3a027c5a4479bf7ac11aece0085081
Vlad <vladysalv.melnychuk.k@gmail.com>
2017-11-16 19:35:56 +0200

    reactivity

--- client/main.js
+++ client/main.js
@@ -8,47 +8,29 @@ import './main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
         template = this;
-      template.counter = new ReactiveVar(0);
         
   Tracker.autorun(() => {
-             Meteor.subscribe('cassandra', function(){
-         console.log("sss", cassandra.find().fetch());
-         let counter = cassandra.find().fetch()[0].clicks;
-         template.counter.set(counter);
+          
+  })
+   Meteor.subscribe('cassandra', function(){
      });
-    })
-    //console.log(this.counter);
-    // console.log(Messages.find().fetch())
 });
 
 Template.hello.helpers({
   
     counter() {
-        //return Template.instance().counter.get();
-        //console.log(cassandra.find().fetch());
-        let counter = cassandra.find().fetch();
-            counter = counter.sort((a,b)=> { 
-            return new Date(a.date).getTime() - new Date(b.date).getTime() 
-        });
-            console.log(counter);
+        const counter = cassandra.find().fetch(); 
+        console.log(counter);
         return counter.length
     }
 });
 
 Template.hello.events({
     'click button'(event, instance) {
-        //const t = Template.instance();
-        const counter = Template.instance().counter.get()+1;
-       
-        //cassandra.insert({increment: counter})
-
-        //instance.counter.set(instance.counter.get() + 1);
-        console.log(cassandra.find().fetch(), counter);
-        Meteor.call("add", {clicks:counter, message:"fin"}, (e,r)=>{
-            console.log("er",cassandra.find().fetch(),r);
-            if(r.succes == 200){
-               instance.counter.set(counter); 
-            }
+        //console.log("before.add",cassandra.find().fetch());
+        Meteor.call("add", {clicks:cassandra.find().fetch().length, message:"fin"}, (e,r)=>{
+            //console.log("after.add",cassandra.find().fetch());
+            console.log(r,e)
         });
     },
 });
--- server/main.js
+++ server/main.js
@@ -14,274 +14,96 @@ import {
     uuid
 } from 'cassmask';
 
-
-
+const cassandra = require('cassandra-driver');
+const distance = cassandra.types.distance;
 
 const ip = '35.192.70.53';
 
-// cassandra connect config
-const config = {
+const cassClient = new cassandra.Client({
     contactPoints: [ip + ':9160', ip + ':9042'],
-    //protocolOptions: { port: 9042 },
-    queryOptions: {
-        //consistency: 1
-    },
-    keyspace: 'dev'
-};
-let holder = [];
-//model (in es5)
-var Model = cassmask.model('second_clicker', new cassmask.Schema({
-  uid: {
-    type: cassmask.UUID,
-    default: uuid()
-  },
-  time_uid: {
-    type: cassmask.TIMEUUID,
-    default: now()
-  },
-  date: {
-    type: cassmask.TIMESTAMP,
-    default: toTimeStamp(now())
-  },
-  clicks: {
-    type: cassmask.INT,
-    default:0
-  },
-  message:{
-    type: cassmask.TEXT,
-    default:""
-  },
-  // Primary Keys (Partition & Clustering Column)
-  keys: ['uid', 'time_uid']  
-}
-));
+    keyspace: 'dev',
 
-
-
-// Model.validate('increment', function(increment, next) {
-//     if (increment >= 0) next();
-//     else next('increment value not large enough!');
-// });
+});
 
 Meteor.startup(() => {
-    // Model.create([{
-    //     increment: 1,
-    // }]);
-   
+   /* cassClient.stream(select)
+      .on('readable', function () {
+        console.log("stream", this);
+    });*/
+    
+
+
+
 });
-const store = (function(){
+
+
+const stremTest = (function(){
+    const select = 'SELECT count(*) FROM clicks3';
+    cassClient.stream(select)
+        .on('readable', function () {
+         console.log('readable:', this.read());
+      }).on('error', err => {
+                if(err) {
+                    this.error = err;
+                }
+            })
+})();
+
+const simpleObserver = (function(){
     let data = [];
     return {
         subscribe:(script, ready)=>{
-            script(Random.id(), {message:"1111"});
+            script({message:"subscribe"});
             ready();
             data.push([script, ready]);
         },
-        run:(id, nexData)=>{
+        run:(msg)=>{
             //console.log(data.length);
             data.forEach((script)=>{
-                script[0](Random.id(), nexData);
+                script[0](msg);
                 script[1]();
-
             });
         },
-        get:()=>{
-            return data
+        isEmpty:()=>{
+           return !data.length 
         }
     }
 })();
+
+
 Meteor.publish('cassandra', function() {
     let self = this;
-   /* cassmask.connect(config, (err, result) => {
 
-   Model.findOne().seam().subscribe( // Every next argument will be an Entity object for every query executed
-        x => {
-            holder.push(x);
-            console.log("ss>", x.toJSON());
-            self.added('cassandra', x.toJSON().id+'', {clicks:x.toJSON()})
-        },
-        err => {
-            console.log("name:>>> " + err.name)
-            console.log(err)
-        },
-        () => {
-            self.ready();
-        }
-    );
-    Model.findOne().find().seam().subscribe(
-                tests => {
-                     holder.push(tests);
-                     console.log("find>", tests.length, holder);
-                     let arr = tests;
-                     arr.forEach((obj)=> {
-                         self.added('cassandra', obj.toJSON().id+'', {
-                             clicks:obj.toJSON().clicks,
-                             message:obj.toJSON().message,
-                             date:obj.toJSON().date
-                         })
-                     });
-                    
-                },
-                err => console.log(err),
-                () => {
-                    self.ready();
-                    done();
-                }
-         );
-    });
-*/
-    store.subscribe((id, data)=>{
-        self.added('cassandra', id+'', data);
+    simpleObserver.subscribe((msg)=>{
+         //console.log('notify', msg);
+         const select = 'SELECT * FROM clicks3';
+         if(msg.message == 'subscribe' || msg.message == 'pool'){
+         cassClient.execute(select).then(result =>{
+           // console.log("result",result.rows.length);
+            if(msg.message == 'subscribe'){
+                result.rows.forEach((row)=> self.added('cassandra', row.id+'', row) );
+            }
+            
+         });
+         }else{
+            self.added('cassandra', msg.nextData.id+'', msg.nextData)
+         }
     },()=>{
          self.ready();
     });
+
 });
 
-// ALTERNATIVE PACKAGE TO CHECK IF CASSANDRA IS UP AND RUNNING. DOES NOT SUPPORT REACTIVITY ONLY CQL QUERIES
-// const cassandra = require('cassandra-driver');
-// const client = new cassandra.Client({ contactPoints: [ip+':9042',ip+':9160'], keyspace: 'dev' });
-// const query = 'CREATE TABLE dev.cassandra2 (id:uuid, uuid: uuid, increment:int)';
-// client.connect()
-//   .then(function () {
-//     console.log('Connected to cluster with %d host(s): %j', client.hosts.length, client.hosts.keys());
-//     console.log('Keyspaces: %j', Object.keys(client.metadata.keyspaces));
-//     console.log('Shutting down');
-//     client.query(query,function(){
-//         console.log('query executed?')
-//     })
-//     return client.shutdown();
-//   })
-//   .catch(function (err) {
-//     console.error('There was an error when connecting', err);
-//     return client.shutdown();
-// });
 
 Meteor.methods({
-    "add":function(obj){
-       /*Model.create([obj]).findOne().find().seam().subscribe(
-            model => {
-                      holder.push(model);
-                     console.log("add>", model.length, holder.length);
-                    // console.log(holder[0]);    
-                },
-                err => console.log(err),
-                () => {
-                    done();
-                }
-           );*/
-         /*  console.log('method add start');
-             console.log("_________________________");
-                console.log(this, next, err);
-                console.log("_________________________");
-            Model.post('create', function(next, err) {
+    "add":async (obj)=>{
+          self = this;
+          const nextData = {id:Random.id(), clicks:obj.clicks, date:new Date()};
+          const insert =`INSERT INTO clicks3 (id, clicks, date) VALUES ('${nextData.id}', ${nextData.clicks}, ${Date.parse(nextData.date) })`;
+          let call = await cassClient.execute(insert);
+          simpleObserver.run({message:"insert", nextData});
 
-                Model.Events.emit('create', this);
-                next(this);
-            });*/
-
-             store.run( store.get().length+1+'', {message:"Hello Creature!"});
-          return {succes:200}
+          return call     
     }
 });
 
-
-
-/*
-import {
-    Meteor
-} from 'meteor/meteor';
-import {
-    Mongo
-} from 'meteor/mongo'
-
-//const cassmask = require('cassmask');
-import * as cassmask from 'cassmask';
-import {
-    toTimeStamp,
-    now,
-    uuid
-} from 'cassmask';
-
-const ip = '35.192.70.53';
-const cass = require('cassandra-driver');
-const cassClient = new cass.Client({
-    contactPoints: [ip + ':9160', ip + ':9042'],
-    keyspace: 'dev' }
-);
-
-
-
-const store = (function(){
-    let data = { };
-    const count = (sub)=>{
-        const ct ='SELECT count(*) FROM clicks';
-        cassClient.execute(ct).then(result =>{
-         console.log('sc', result.rows);
-         sub.changed('cassandra', '73', {count:result.rows[0].count.low});
-         sub.ready(); 
-     })
-      return data.count;  
-    }
-    return {
-        get:()=>{
-            return data
-        },
-        set:(name,val)=>{
-            data[name] = val;
-        },
-        count
-    }
-})();
-// Model.validate('increment', function(increment, next) {
-//     if (increment >= 0) next();
-//     else next('increment value not large enough!');
-// });
-
-Meteor.startup(() => {
-    // Model.create([{
-    //     increment: 1,
-    // }]);
-   
-})
-Meteor.publish('cassandra', function() {
-   let sub = this;
-
-      sub.added('cassandra', '73', {count:0});
-
-      store.count(sub); 
-      //sub.ready();
-});
-
-// ALTERNATIVE PACKAGE TO CHECK IF CASSANDRA IS UP AND RUNNING. DOES NOT SUPPORT REACTIVITY ONLY CQL QUERIES
-// const cassandra = require('cassandra-driver');
-// const client = new cassandra.Client({ contactPoints: [ip+':9042',ip+':9160'], keyspace: 'dev' });
-// const query = 'CREATE TABLE dev.cassandra2 (id:uuid, uuid: uuid, increment:int)';
-// client.connect()
-//   .then(function () {
-//     console.log('Connected to cluster with %d host(s): %j', client.hosts.length, client.hosts.keys());
-//     console.log('Keyspaces: %j', Object.keys(client.metadata.keyspaces));
-//     console.log('Shutting down');
-//     client.query(query,function(){
-//         console.log('query executed?')
-//     })
-//     return client.shutdown();
-//   })
-//   .catch(function (err) {
-//     console.error('There was an error when connecting', err);
-//     return client.shutdown();
-// });
-
-Meteor.methods({
-    "add":function(obj){
-        const insert =`INSERT INTO clicks (id) VALUES (${obj.clicks})`;
-        cassClient.execute(insert).then(result =>{
-         //console.log('sc', result)
-         store.set('table',result.rows);
-         store.count();
-     });
-       return {succes:200, count:store.get().count}
-    }
-})
-
-
-*/
